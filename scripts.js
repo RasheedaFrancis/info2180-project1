bc2fe05714352e523f3a8d31429195e0c23b43a9
Rasheeda Francis <rasheedafrancis02@gmail.com>
2023-11-05 18:56:31 -0600

    Edited files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,25 @@
 /* Add your JavaScript to this file */
+
+window.onload = loadPage;
+
+function loadPage(){
+    let submit = document.querySelectorAll(".btn")[1];
+        
+    submit.addEventListener('click', clicker);
+}
+
+function clicker(event){
+    event.preventDefault();
+
+    const emailAddress = document.getElementById("email").value;
+
+    if(emailAddress!=""){
+        document.getElementsByClassName("message")[0].textContent = alert("Thank You! Your email address " +emailAddress+ " has been added to our mailing list");
+    }else{
+        document.getElementsByClassName("message")[0].textContent = ""
+        document.getElementsByClassName("message")[0].textContent =alert("Please enter a valid email address")
+    }
+}
+
+
+
